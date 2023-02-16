469a139023e63f5f856ea2cd292d8de63fc1c9c2
05j09a97b <axesmex@gmail.com>
2023-02-16 16:42:20 +0700

    VacQ

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const cookieParser = require('cookie-parser');
 
 
 //Load env vars
@@ -11,11 +12,17 @@ connectDB();
 
 //route files
 const hospitals = require('./routes/hospitals');
+const auth = require('./routes/auth');
 const app = express();
+
+
+//add cookie parser
+app.use(cookieParser());
 
 //add body parser
 app.use(express.json());
 app.use('/api/v1/hospitals',hospitals);
+app.use('/api/v1/auth',auth);
 
 const PORT = process.env.PORT || 4000;
 const server = app.listen(PORT,console.log('Server running in',process.env.NODE_ENV,'mode on port',PORT));
